5335f7eff8f4e5bc000b1e5acc56e49406fdab7d
byteresearchcla <chengshanbo@bytedance.com>
2024-07-20 11:26:47 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# RealSI
+
+RealSI: Open Benchmark for Simultaneous Interpretation in Real-world Scenarios
+
+![RealSI](resource/ST.jpeg)
+
+For details of the dataset, please refer to the [Technical Report](https://www.arxiv.org) of Cross Language Agent-Simultaneous Interpretation, `CLASI`.
+
+## Download
+
+Please follow the instructions to download the dataset.
+
+```bash
+pip3 install -r requirements.txt
+mkdir data/en2zh/audio data/zh2en/audio
+
+python3 toolkits/download_audio.py
+```
+
+## Introduction
+
+`RealSI` is a benchmark for Simultaneous Interpretation(SI) in real-world scenarios. This project intends to release a public test set for SI, in order to evaluate the speech translation performance of the model in difficult scenarios such as long audio, speakers not being fully prepared, and heavy accents.
+
+
+## Data Construction
+
+Audio data are collected from public videos, covering 2 languages and 10 different topics. We cut out video clips of 3-7 minutes, and label the timestamps, transcripts and translations of speech in the video.
+
+| Domain   | Duration - zh2en | #Segments - zh2en | Duration - en2zh | #Segments - en2zh |
+|:---------------|:-----------------:|:--------------:|:-----------------:|:-------------------:|
+| Technology | 5:23 | 51 | 3:25 | 31 |
+| Healthcare | 3:16 | 30 | 3:34 | 22 |
+| Education  | 4:56 | 48 | 5:00 | 41 |
+| Finance    | 5:22 | 29 | 5:01 | 40 |
+| Law        | 4:38 | 49 | 4:48 | 29 |
+| Environment| 4:18 | 34 | 4:24 | 31 |
+| Entertainment | 5:16 | 53 | 5:12 | 39|
+| Science | 4:47 | 37 | 5:11 | 35 |
+| Sports | 5:22 | 33 | 3:25 | 58 |
+| Art    | 7:54 | 67 | 4:17 | 21 |
+| ***Total*** | ***51:12*** | ***431*** | ***44:17*** | ***347*** |
+
+**DISCLAIMER:** ***We do not own the copyright of the
+videos and only release our annotation together with the publicly available website links of the corresponding
+videos. If anyone believes that the content constitutes infringement, please contact us. We will remove the
+relevant content as soon as possible once confirmed. Any content in this dataset is available for educational and informational purposes only. You are solely responsible for legal liability arising from your improper use of the dataset content. Refer to [License](#lic)***
+
+
+## Structure Walkthrough
+
+We save each audio in separate files, each file contains information of id and duration of the audio. The utterances are grouped in semantic segments, which convey complete semantic for translation. When conducting human evaluation, we recommend evaluating the model prediction results according to this segmentation.
+
+We show the data structure by taking `en2zh-01-tech` as example.
+
+```json
+{
+  "vid": "en2zh-01-tech",
+  "duration": 205000,
+  "segment": [
+    {
+      "start_time": 0,
+      "end_time": 5790,
+      "utterance": [
+        {
+          "sub_start_time": 0,
+          "sub_end_time": 2810,
+          "sub_src_text": "Round-robin um um load balancing scheme,",
+          "sub_trg_text": "轮询负载均衡方案。",
+          "term": [
+            {
+                "src": "Round-robin",
+                "trg": "轮询"
+            },
+            ... ...
+          ]
+        },
+        ... ...
+      ],
+    },
+    ... ...
+  ]
+}
+```
+
+
+## <a name="lic"></a>License
+
+This dataset is released under the Creative Commons Attribution 4.0 International License. Please refer to [CC-BY-4.0](https://creativecommons.org/licenses/by/4.0/) for more details.
+
+
---
+++ toolkits/download_audio.py
@@ -0,0 +1,83 @@
+import os
+import io
+import time
+from pytube import YouTube
+from pydub import AudioSegment
+
+SAMPLE_RATE=16000
+
+
+def download_youtube_video(url, output_path, start_time, end_time, retries=30, delay=5, target_format='FLAC'):
+    while retries > 0:
+        try:
+            yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+            stream = yt.streams.filter(only_audio=True).order_by("abr").desc().first()
+            buffer = io.BytesIO()
+            stream.stream_to_buffer(buffer)
+            buffer.seek(0)
+            audio_data = AudioSegment.from_file(buffer, format='webm')
+            st, et = time_to_milliseconds(start_time), time_to_milliseconds(end_time)
+            audio_data = audio_data[st:et]
+            audio_data = audio_data.set_frame_rate(SAMPLE_RATE)
+            audio_data.export(output_path, format=target_format, parameters=["-ar", str(SAMPLE_RATE), "-ac", "1"])
+            return 
+        except Exception as e:
+            print(f"Download failed: {e}. Retrying in {delay} seconds...")
+            time.sleep(delay)
+            retries -= 1
+    raise Exception(f"Failed to download video after multiple attempts: {url}")
+
+
+def time_to_milliseconds(time_str):
+    h, m, s = map(float, time_str.strip().split(':'))
+    return int((h * 3600 + m * 60 + s) * 1000)
+
+
+video_list_zh = [
+    ("zh2en-01-tech", "https://www.youtube.com/watch?v=lhcQkrkTUhc", "00:00:43 - 00:06:05.75"),
+    ("zh2en-02-health", "https://www.youtube.com/watch?v=D2kmp5jzGH8", "00:14:35 - 00:17:52"),
+    ("zh2en-03-edu", "https://www.youtube.com/watch?v=K-5cM2gaKUU", "00:03:19.80 - 00:08:15.80"),
+    ("zh2en-04-fin", "https://www.youtube.com/watch?v=1bxJKYlP2SQ", "00:44:09 - 00:49:31.50"),
+    ("zh2en-05-law", "https://www.youtube.com/watch?v=faDmOWyGNxk", "00:07:49 - 00:12:27"),
+    ("zh2en-06-env", "https://www.youtube.com/watch?v=EpnR9GZdoDw", "00:01:00.39 - 00:05:18"),
+    ("zh2en-07-ent", "https://www.youtube.com/watch?v=07RvKD801Bo", "00:00:11.80 - 00:05:27.75"),
+    ("zh2en-08-sci", "https://www.youtube.com/watch?v=f5HARGtfXSQ", "00:01:40 - 00:06:27"),
+    ("zh2en-09-sport", "https://www.youtube.com/watch?v=hp0jN1iawSQ", "00:46:35 - 00:52:04"),
+    ("zh2en-10-art", "https://www.youtube.com/watch?v=98aNJZ0UV-c", "01:14:17 - 01:20:37"),
+]
+
+video_list_en = [
+    ("en2zh-01-tech",   "https://www.youtube.com/live/_K-eupuDVEc", "00:37:30 - 00:40:55 "),
+    ("en2zh-02-health", "https://www.youtube.com/watch?v=m_OKi0_YrrM", "00:15:46 - 00:19:20"),
+    ("en2zh-03-edu",    "https://www.youtube.com/watch?v=mZHOTfSrox0", "00:00:38 - 00:05:38"),
+    ("en2zh-04-fin",    "https://www.youtube.com/watch?v=BPK_qzeH_yk", "00:17:24 - 00:22:25"),
+    ("en2zh-05-law",    "https://www.youtube.com/watch?v=o6bO3zzrBp0", "00:00:57 - 00:05:55"),
+    ("en2zh-06-env",    "https://www.youtube.com/watch?v=jlJijwgtgzI", "00:02:05 - 00:06:29"),
+    ("en2zh-07-ent",    "https://www.youtube.com/watch?v=fi1Ke6yAVP4", "01:22:48 - 01:28:00"),
+    ("en2zh-08-sci",    "https://www.youtube.com/watch?v=hRz0BBmPNDs", "00:28:00 - 00:33:11"),
+    ("en2zh-09-sport",  "https://www.youtube.com/watch?v=sKDwRSuCDZM", "00:24:35 - 00:28:00"),
+    ("en2zh-10-art",    "https://www.youtube.com/watch?v=FbZ_MOWLMu8", "00:20:28 - 00:24:45"),
+]
+
+if __name__ == "__main__":
+    for vid, url, duration in video_list_en:
+        out_path = f"data/en2zh/audio/{vid}.flac"
+        if os.path.exists(out_path):
+            continue
+        st, et = duration.split("-", 1)
+        print(f"Downloading {vid} to {out_path}")
+        try:
+            download_youtube_video(url, out_path, st, et)
+        except Exception as ex:
+            print("Download failed! Due to", ex)
+
+    for vid, url, duration in video_list_zh:
+        out_path = f"data/zh2en/audio/{vid}.flac"
+        if os.path.exists(out_path):
+            continue
+        st, et = duration.split("-", 1)
+        print(f"Downloading {vid} to {out_path}")
+        try:
+            download_youtube_video(url, out_path, st, et)
+        except Exception as ex:
+            print("Download failed! Due to", ex)
